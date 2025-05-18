f83f3af1a3cace07f722282e8279f89af31c5e70
omairKhan99 <omairtkhan@gmail.com>
2025-05-17 20:37:55 -0700

    Initial Commit

---
+++ CoffeeFinder/CoffeeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeFinderApp.swift
+//  CoffeeFinder
+//
+//  Created by Omair Khan on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
